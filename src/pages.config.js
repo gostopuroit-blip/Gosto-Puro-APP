d57673e89fb6a478e248a743204d20e0020656da
Brandom Fernandes <gostopurobrandom@gmail.com>
2026-02-19 14:41:50 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -47,24 +47,24 @@
  *
  * The mainPage value must match a key in the PAGES object exactly.
  */
-import Recipes from './pages/Recipes';
-import RecipeDetail from './pages/RecipeDetail';
 import Folders from './pages/Folders';
-import Planner from './pages/Planner';
-import ShoppingList from './pages/ShoppingList';
-import Profile from './pages/Profile';
 import Home from './pages/Home';
+import Planner from './pages/Planner';
+import Profile from './pages/Profile';
+import RecipeDetail from './pages/RecipeDetail';
+import Recipes from './pages/Recipes';
+import ShoppingList from './pages/ShoppingList';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Recipes": Recipes,
-    "RecipeDetail": RecipeDetail,
     "Folders": Folders,
-    "Planner": Planner,
-    "ShoppingList": ShoppingList,
-    "Profile": Profile,
     "Home": Home,
+    "Planner": Planner,
+    "Profile": Profile,
+    "RecipeDetail": RecipeDetail,
+    "Recipes": Recipes,
+    "ShoppingList": ShoppingList,
 }
 
 export const pagesConfig = {
