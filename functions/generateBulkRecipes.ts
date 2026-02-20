308d64a06930c21fc054638025ae60fd2d42b45e
Brandom Fernandes <gostopurobrandom@gmail.com>
2026-02-20 21:50:35 +0000

    File changes

--- functions/generateBulkRecipes.ts
+++ functions/generateBulkRecipes.ts
@@ -67,8 +67,7 @@ Deno.serve(async (req) => {
 
     return Response.json({ 
       success: true, 
-      totalGenerated, 
-      details: results 
+      totalGenerated
     });
   } catch (error) {
     return Response.json({ error: error.message }, { status: 500 });
