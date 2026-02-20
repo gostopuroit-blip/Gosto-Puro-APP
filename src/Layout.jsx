cc53dcc8a439584dfd18f646cf8e440afa1efbb5
Brandom Fernandes <gostopurobrandom@gmail.com>
2026-02-20 16:47:48 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -108,7 +108,7 @@ export default function Layout({ children, currentPageName }) {
       <img src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/699707f25ff5e371dc9a1c99/e235954e8_1200x801000x80px1.png" alt="Gosto Puro" className="w-full object-contain" />
       
       <main className="flex-1 pb-24 max-w-lg mx-auto w-full">
-        <div className="flex justify-end px-4 pt-2">
+        <div className="flex justify-end px-4">
           <PremiumBadge user={user} />
         </div>
         <AnimatePresence mode="wait">
