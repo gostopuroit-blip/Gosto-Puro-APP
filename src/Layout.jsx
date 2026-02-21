c909a2dd6ec7d3fe371edc627878625687c9fd43
Brandom Fernandes <gostopurobrandom@gmail.com>
2026-02-21 12:17:20 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -108,7 +108,7 @@ export default function Layout({ children, currentPageName }) {
       `}</style>
 
       <div className="flex justify-center w-full flex-shrink-0">
-        <img src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/699707f25ff5e371dc9a1c99/c0529d754_headerapp.png" alt="Gosto Puro" className="w-1/3 max-w-[160px] object-contain" />
+        <img src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/699707f25ff5e371dc9a1c99/c0529d754_headerapp.png" alt="Gosto Puro" style={{ width: '120px', height: 'auto', display: 'block' }} />
       </div>
       
       <main className="flex-1 pb-24 max-w-lg mx-auto w-full">
