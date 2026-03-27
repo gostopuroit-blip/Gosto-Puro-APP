7b9dbc107dc6780f947282315b5333ad62e58fce
Brandom Fernandes <gostopurobrandom@gmail.com>
2026-03-27 13:30:59 +0000

    File changes

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -45,7 +45,7 @@ export default function Recipes() {
   }, []);
 
   const loadRecipes = async () => {
-    const data = await base44.entities.Recipe.filter({ status: "pubblicata" }, "-created_date", 1000);
+    const data = await base44.entities.Recipe.filter({ status: "pubblicata" }, "-created_date", 5000);
     setRecipes(data);
     setLoading(false);
   };
